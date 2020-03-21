2a87c1b8ed084198bba7861cf014f417fe1a22f9
ShroXd <ShroXd@gmail.com>
2020-03-21 15:07:58 +0800

    增加用户验证模块

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,14 @@ from flask import Flask
 
 
 def create_app():
-    from . import login
+    from . import auth
     app = Flask(__name__, instance_relative_config=True)
 
-    app.register_blueprint(login.bp)
+    # 支持中文显示
+    app.config['JSON_AS_ASCII'] = False
+    # session 的秘钥
+    app.secret_key = 'djskla'
+
+    app.register_blueprint(auth.bp)
 
     return app
---
+++ app/auth.py
@@ -0,0 +1,59 @@
+from flask import (Blueprint, request, jsonify, session)
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from . import db
+
+bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+
+@bp.route('/register', methods=('GET', 'POST'))
+def register():
+    if request.method == 'POST':
+        username = request.form.get('username', None)
+        password = request.form.get('password', None)
+        user = db.get_db().users
+        msg = None
+
+        if not username:
+            msg = '请输入用户名'
+        elif not password:
+            msg = '请输入密码'
+        elif user.find_one({'username': username}) is not None:
+            msg = '用户 {} 已注册'.format(username)
+
+        if msg is None:
+            user.insert({
+                'username': username,
+                'password': generate_password_hash(password)
+            })
+            return jsonify({'msg': '注册成功'})
+        else:
+            return jsonify({'msg': msg})
+
+
+@bp.route('/login', methods=('POST', 'GET'))
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username', None)
+        password = request.form.get('password', None)
+        user = db.get_db().users
+        msg = None
+        user = user.find_one({'username': username})
+
+        if user is None:
+            msg = '用户名错误'
+        elif not check_password_hash(user['password'], password):
+            msg = '密码错误'
+
+        if msg is None:
+            session.clear()
+            session['user_id'] = user['username']
+            return {'msg': '登录成功'}
+        else:
+            return {'msg': msg}
+
+
+@bp.route('/logout')
+def logout():
+    session.clear()
+    return {'msg': '登出成功'}
