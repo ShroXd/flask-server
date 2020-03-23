820451e392d33a800494ce27668d20ec768c35bf
ShroXd <ShroXd@gmail.com>
2020-03-23 20:54:17 +0800

    优化书籍获取各级接口

--- app/novel/views.py
+++ app/novel/views.py
@@ -1,3 +1,5 @@
+import math
+
 from bson import json_util
 from flask import (Blueprint, request)
 
@@ -16,10 +18,13 @@ def books():
     list_limit = int(request.values.get('listLimit'))
     collections = extensions.get_db().books
 
-    results = collections.find({}, {'_id': False}).skip(list_page * list_limit).limit(list_limit)
-    results_list =  [x for x in results]
+    results = collections.find({}, {
+        '_id': False
+    }).skip(list_page * list_limit).limit(list_limit)
+    total = math.floor(results.count() / list_limit)
+    results_list = [x for x in results]
 
-    return {'msg': '请求成功', 'data': results_list}
+    return {'msg': '请求成功', 'data': results_list, 'total': total}
 
 
 @blueprint.route('/chapters', methods=['POST'])
@@ -29,20 +34,23 @@ def chapters():
     book_name = str(request.values.get('bookName'))
     collections = extensions.get_db().chapters
 
-    results = collections.find({'bookName': book_name}, {'_id': False})
-    results_list = [x for x in results]
+    results = collections.find_one({'bookName': book_name}, {'_id': False})
+    # results_list = [x for x in results]
 
-    return {'msg': '请求成功', 'data': results_list}
+    return {'msg': '请求成功', 'data': results}
 
 
 @blueprint.route('/contents', methods=['POST'])
 @utils.token_required
-@utils.params_check(['chapterId'])
+@utils.params_check(['bookName', 'chapterId'])
 def contents():
+    book_name = str(request.values.get('bookName'))
     chapter_id = str(request.values.get('chapterId'))
     collections = extensions.get_db().contents
 
-    results = collections.find({'chapterId': chapter_id}, {'_id': False})
-    results_list = [x for x in results]
+    results = collections.find_one({
+        'bookName': book_name,
+        'chapterId': chapter_id
+    }, {'_id': False})
 
-    return {'msg': '请求成功', 'data': results_list}
+    return {'msg': '请求成功', 'data': results}
