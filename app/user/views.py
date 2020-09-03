d7d6f6f8d07462715c25c4effdf4e524efe71e68
ShroXd <ShroXdF@gmail.com>
2020-09-03 21:24:53 +0800

    refactor (user info): 重构获取用户信息接口

--- app/user/views.py
+++ app/user/views.py
@@ -2,7 +2,7 @@ import uuid
 
 from flask import (Blueprint, request)
 
-from app import extensions
+from app import app
 from app import utils
 
 blueprint = Blueprint('user', __name__, url_prefix='/user')
@@ -13,11 +13,17 @@ blueprint = Blueprint('user', __name__, url_prefix='/user')
 def info():
     token = request.headers['Authorization']
     user_data = utils.verify_token(token)
-    collections = extensions.get_db().users
+    users = app.mongo.db.users
 
-    result = collections.find_one({'userId': uuid.UUID(user_data['userId'])})
+    result = users.find_one({'userId': uuid.UUID(user_data['userId'])})
+
+    if result is None:
+        return {
+            "message": "用户不存在"
+        }, utils.http_code['BadRequest']
+
     return {
-        'msg': '请求成功',
+        'message': '请求成功',
         'data': {
             'username': result['username'],
             'userId': result['userId']
