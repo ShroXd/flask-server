38641d0cccba4d1306b3778960249e780139d0f9
ShroXd <ShroXdF@gmail.com>
2020-09-03 21:36:35 +0800

    refactor (auth test): 集中管理测试用户信息

--- test/unit/test_auth.py
+++ test/unit/test_auth.py
@@ -1,52 +1,36 @@
 from test import utils
 from app.utils import http_code
 
-user_complete_info = {
-    "username": "test_username",
-    "password": "test_password"
-}
-user_incomplete_info = {
-    "username": "test_username"
-}
-user_wrong_password_info = {
-    "username": "test_username",
-    "password": "wrong_password"
-}
-user_not_found_info = {
-    "username": "not_found",
-    "password": "password"
-}
-
 
 def test_register(client, app):
     # 缺少参数
     assert client.post("/auth/register").status_code == http_code['BadRequest']
-    assert client.post("/auth/register", data=user_incomplete_info).status_code == http_code['BadRequest']
+    assert client.post("/auth/register", data=utils.user_incomplete_info).status_code == http_code['BadRequest']
 
     # 成功注册
-    assert client.post("/auth/register", data=user_complete_info).status_code == http_code['Created']
+    assert client.post("/auth/register", data=utils.user_complete_info).status_code == http_code['Created']
 
     # 用户名重复
-    assert client.post("/auth/register", data=user_complete_info).status_code == http_code['Conflict']
+    assert client.post("/auth/register", data=utils.user_complete_info).status_code == http_code['Conflict']
 
     # 清理数据库
     utils.clear_db()
 
 
 def test_login(client, app):
-    response = client.post('/auth/register', data=user_complete_info)
+    client.post('/auth/register', data=utils.user_complete_info)
 
     # 缺少参数
     assert client.post("/auth/register").status_code == http_code['BadRequest']
 
     # 密码错误
-    assert client.post("/auth/login", data=user_wrong_password_info).status_code == http_code['Forbidden']
+    assert client.post("/auth/login", data=utils.user_wrong_password_info).status_code == http_code['Forbidden']
 
     # 用户未注册
-    assert client.post("/auth/login", data=user_not_found_info).status_code == http_code['Not Acceptable']
+    assert client.post("/auth/login", data=utils.user_not_found_info).status_code == http_code['Not Acceptable']
 
     # 成功登录
-    assert client.post("/auth/login", data=user_complete_info).status_code == http_code['OK']
+    assert client.post("/auth/login", data=utils.user_complete_info).status_code == http_code['OK']
 
     # 清理数据库
     utils.clear_db()
