767a4cf76a1e508ca50322c257dfa44eb526b138
ShroXd <ShroXdF@gmail.com>
2020-09-04 14:06:04 +0800

    test (novels): 为获取书籍 api 增加测试

---
+++ test/unit/test_novel.py
@@ -0,0 +1,16 @@
+from app.utils import http_code
+
+
+def test_novels(client, app):
+    # 缺少参数
+    assert client.get("/novel/books").status_code == http_code['BadRequest']
+
+    # 错误参数
+    assert client.get("/novel/books?listPage=0&&listLimit=0").status_code == http_code['BadRequest']
+    assert client.get("/novel/books?bookName=理想的女儿是世界最强，你也愿意宠爱吗？&&listPage=0&&listLimit=0").status_code == http_code[
+        'BadRequest']
+
+    # 正确请求
+    assert client.get("/novel/books?listPage=1&&listLimit=10").status_code == http_code['OK']
+    assert client.get("/novel/books?bookName=理想的女儿是世界最强，你也愿意宠爱吗？&&listPage=1&&listLimit=10").status_code == http_code[
+        'OK']
